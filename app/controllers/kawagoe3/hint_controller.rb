1a58107413f8ce91f01e5a68680b088c517c6ae0
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-02-25 10:20:46 +0900

    コメントアウトを削除

--- app/controllers/kawagoe3/hint_controller.rb
+++ app/controllers/kawagoe3/hint_controller.rb
@@ -1,14 +1,7 @@
 class Kawagoe3::HintController < ApplicationController
-  # before_filter :basic
   http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
   def index
   end
 
-  # private
-  # def basic
-  #   authenticate_or_request_with_http_basic do |user, pass|
-  #     user == ENV['BASIC_AUTH_USERNAME'] && pass == ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
-  #   end
-  # end
 end
