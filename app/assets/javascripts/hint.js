4a5d63d518abfbf7c402ddc75c8f080f16eb27de
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-03-02 00:03:26 +0900

    ヒントの文言を変更し、画像をレスポシンブ化

--- app/assets/javascripts/hint.js
+++ app/assets/javascripts/hint.js
@@ -1,96 +1,60 @@
 $(function(){
-    $("#h1-2").css("display", "none");
-    $("#h1-3").css("display", "none");
-    $("#h1-4").css("display", "none");
-    $("#h2-1").css("display", "none");
-    $("#h2-2").css("display", "none");
-    $("#h2-3").css("display", "none");
-    $("#h2-4").css("display", "none");
-    $("#h2-5").css("display", "none");
-    $("#h3-1").css("display", "none");
-    $("#h3-2").css("display", "none");
-    $("#h3-3").css("display", "none");
-    $("#h3-4").css("display", "none");
-    $("#h3-5").css("display", "none");
+    $("#h1-5-2").css("display", "none");
+    $("#h1-6-2").css("display", "none");
+    $("#h2-5-2").css("display", "none");
+    $("#h2-6-2").css("display", "none");
+    $("#h3-1-2").css("display", "none");
+    $("#h3-2-2").css("display", "none");
+    $("#h3-3-2").css("display", "none");
 
-    var num = 0;
+    var num1 = 0;
+    var num2 = 0;
+    var num3 = 0;
+    var num4 = 0;
+    var num5 = 0;
+    var num6 = 0;
+    var num7 = 0;
 
-    $("#h1-1").click(function(){
-      if (num == 0){
-        $("#h1-2").toggle();
-        num = 1;
+    $("#h1-5-1").click(function(){
+      if (num1 == 0){
+        $("#h1-5-2").toggle();
+        num1 = 1;
       }
     });
-    $("#h1-2").click(function(){
-      if (num == 1){
-        $("#h1-3").toggle();
-        num = 2;
+    $("#h1-6-1").click(function(){
+      if (num2 == 0){
+        $("#h1-6-2").toggle();
+        num2 = 1;
       }
     });
-    $("#h1-3").click(function(){
-      if (num == 2){
-        $("#h1-4").toggle();
-        num = 3;
+    $("#h2-5-1").click(function(){
+      if (num3 == 0){
+        $("#h2-5-2").toggle();
+        num3 = 1;
       }
     });
-    $("#h1-4").click(function(){
-      if (num == 3){
-        $("#h2-1").toggle();
-        num = 4;
+    $("#h2-6-1").click(function(){
+      if (num4 == 0){
+        $("#h2-6-2").toggle();
+        num4 = 1;
       }
     });
-    $("#h2-1").click(function(){
-      if (num == 4){
-        $("#h2-2").toggle();
-        num = 5;
+    $("#h3-1-1").click(function(){
+      if (num5 == 0){
+        $("#h3-1-2").toggle();
+        num5 = 1;
       }
     });
-    $("#h2-2").click(function(){
-      if (num == 5){
-        $("#h2-3").toggle();
-        num = 6;
+    $("#h3-2-1").click(function(){
+      if (num6 == 0){
+        $("#h3-2-2").toggle();
+        num6 = 1;
       }
     });
-    $("#h2-3").click(function(){
-      if (num == 6){
-        $("#h2-4").toggle();
-         num = 7;
-      }
-    });
-    $("#h2-4").click(function(){
-      if (num == 7){
-        $("#h2-5").toggle();
-         num = 8;
-      }
-    });
-    $("#h2-5").click(function(){
-      if (num == 8){
-        $("#h3-1").toggle();
-         num = 9;
-      }
-    });
-    $("#h3-1").click(function(){
-      if (num == 9){
-        $("#h3-2").toggle();
-         num = 10;
-      }
-    });
-    $("#h3-2").click(function(){
-      if (num == 10){
-        $("#h3-3").toggle();
-         num = 11;
-      }
-    });
-    $("#h3-3").click(function(){
-      if (num == 11){
-        $("#h3-4").toggle();
-         num = 12;
-      }
-    });
-    $("#h3-4").click(function(){
-      if (num == 12){
-        $("#h3-5").toggle();
-         num = 13;
+    $("#h3-3-1").click(function(){
+      if (num7 == 0){
+        $("#h3-3-2").toggle();
+        num7 = 1;
       }
     });
 });
