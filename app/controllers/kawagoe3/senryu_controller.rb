0dcae7bc1397110f1f45ded485d0e72ef2649f6a
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-03-09 00:57:59 +0900

    ヒントページ改修

--- app/controllers/kawagoe3/senryu_controller.rb
+++ app/controllers/kawagoe3/senryu_controller.rb
@@ -1,2 +1,22 @@
 class Kawagoe3::SenryuController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
 end
