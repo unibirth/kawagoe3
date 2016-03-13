b874a830486bb9fb67a78a994f848396dc985da4
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-03-13 11:02:57 +0900

    ヒントページ遷移の際の文言追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     namespace :admin do
     end
     root 'top#index'
-    resources :hint, only: :index
+    resources :hint, only: :index do
+      collection do
+        get :confirm
+      end
+    end
     resources :nazodan, only: :index
     resources :senryu, only: [:index, :new, :create]
   end
