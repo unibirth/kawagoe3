e47be3ec4353da6968c2ae687ba04db5802f7237
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-04-15 21:17:42 +0900

    ヒントページへのユーザー名・パスワードの入力方法変更

--- app/controllers/kawagoe3/hint_controller.rb
+++ app/controllers/kawagoe3/hint_controller.rb
@@ -1,7 +1,10 @@
 class Kawagoe3::HintController < ApplicationController
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+  # http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
 
   def index
+    unless params[:user_name] == "kawagoe3" &&
+       params[:password] == "goemon"
+       redirect_to :back, alert: "ユーザー名かパスワードが違います"
+    end
   end
-
 end
